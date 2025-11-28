d8f4f665cbc0fa84c965f94d547c48aac9e28272
Corentin Briand <cbriand@r-yogi.com>
2025-11-28 20:38:58 +0100

    Add multi app entry

--- vitest.config.js
+++ vitest.config.js
@@ -1,5 +1,21 @@
-import config from "@requirementyogi/vitest-config/node";
+import path from "node:path";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
-export default config({
-    sourceDir: "src",
-});
+/**
+ * Vitest config for a Node app.
+ * @param sourceDir the source directory
+ */
+export default ({ sourceDir = "src" }) => {
+    return defineConfig({
+        plugins: [
+            // Adds support for TypeScript path aliases
+            tsconfigPaths(),
+        ],
+        test: {
+            environment: "node",
+            include: [path.resolve(sourceDir, "**/*.test.ts")],
+            silent: "passed-only"
+        },
+    });
+};
